62f8ad04b80a18cd1f57825926c1189f1b8b7ca8
christophe ridarch <Kuroko@MBPdechristophe.libertelivinglab>
2017-06-10 20:18:19 +0200

    [UPD] clases to cl

--- src/actions/greetings.js
+++ src/actions/greetings.js
@@ -1,13 +1,13 @@
 const agent = require('superagent-promise')(require('superagent'), Promise)
 const formatter = require('../formatter')
-const class = ['CP', 'CE1', 'CE2', 'CM1', 'CM2']
+const cl = ['CP', 'CE1', 'CE2', 'CM1', 'CM2']
 
 export default async function showLanguage(res) {
   console.log('GREETINGS')
 
   const replies = []
   const quickReplies = []
-  class.forEach((l) => {
+  cl.forEach((l) => {
     quickReplies.push({
       name: l,
       value: `I'm in class ${l}`,
